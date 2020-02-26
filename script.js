658f32cb1c12e04390e8d9bbbc2985f2c4c52f0a
aakashsr <aakashsri.dev@gmail.com>
2020-02-26 13:13:43 +0530

    added event listeners to both - select and input element

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 // let futureData = fetch("https://api.exchangerate-api.com/v4/latest/USD");
 
-const currenctEl_one = document.getElementById("currency-one");
+const currencyEl_one = document.getElementById("currency-one");
 const amountEl_one = document.getElementById("amount-one");
 const currencyEl_two = document.getElementById("currency-two");
-const amountEl_two = document.getElementById("currency-two");
+const amountEl_two = document.getElementById("amount-two");
 
 const reateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
 // Fetch exchange rates and update DOM
+
+function calculate() {
+  console.log("ran");
+}
+
+// Event Listeners
+
+currencyEl_one.addEventListener("change", calculate);
+amountEl_one.addEventListener("change", calculate);
+currencyEl_two.addEventListener("change", calculate);
+amountEl_two.addEventListener("change", calculate);
