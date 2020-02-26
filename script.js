45446dff75623eeaa6853dcb1d6327fa95a676be
aakashsr <aakashsri.dev@gmail.com>
2020-02-26 13:09:28 +0530

    selected the required elements

---
+++ script.js
@@ -0,0 +1,11 @@
+// let futureData = fetch("https://api.exchangerate-api.com/v4/latest/USD");
+
+const currenctEl_one = document.getElementById("currency-one");
+const amountEl_one = document.getElementById("amount-one");
+const currencyEl_two = document.getElementById("currency-two");
+const amountEl_two = document.getElementById("currency-two");
+
+const reateEl = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+// Fetch exchange rates and update DOM
