1b7d68f6b64f1706a928696d5c686b3a94d81c97
aakashsr <aakashsri.dev@gmail.com>
2020-02-27 18:54:26 +0530

    added functionality to dynamically convert currency and also displaying them

--- script.js
+++ script.js
@@ -5,14 +5,40 @@ const amountEl_one = document.getElementById("amount-one");
 const currencyEl_two = document.getElementById("currency-two");
 const amountEl_two = document.getElementById("amount-two");
 
-const reateEl = document.getElementById("rate");
+const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
+
+// console.log(amountEl_one.value);
 
 // Fetch exchange rates and update DOM
 
-function calculate() {
-  console.log("ran");
+function calculate(jsonData) {
+  currencyOne = currencyEl_one.value;
+  currencyTwo = currencyEl_two.value;
+  //   amountEl_two.value = jsonData.rates[`${currencyTwo}`];
+
+  // fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
+  //   .then(data => data.json())
+  //   .then(
+  //     jsonData =>
+  //       (amountEl_two.value =
+  //         jsonData.rates[`${currencyTwo}`] * amountEl_one.value)
+  //   );
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
+    .then(data => data.json())
+    .then(jsonData => {
+      const rate = jsonData.rates[currencyTwo];
+      rateEl.innerText = `1 ${currencyOne} =  ${rate} ${currencyTwo}`;
+      amountEl_two.value = (rate * amountEl_one.value).toFixed(2);
+    });
 }
+
+calculate();
+
+// fetch("https://api.exchangerate-api.com/v4/latest/USD")
+//   .then(data => data.json())
+//   .then(calculate);
 
 // Event Listeners
 
